e89269440160025f984a110bd2da218f1c410c9e
ArlanFigueiredo <estudosarlan@gmail.com>
2024-05-26 15:53:36 -0300

    two commit for deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,8 +52,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    // baseURL: 'https://comerce-api.onrender.com/',
-    baseURL: 'http://localhost:3333/',
+    baseURL: 'https://comerce-api.onrender.com/',
+    // baseURL: 'http://localhost:3333/',
   },
   router: {
     base: '/',
